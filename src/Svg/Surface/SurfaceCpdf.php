04d0200568bfa10cec7c29f01f639f0a088f6969
Jayant Soni <j.janu.inos@gmail.com>
2017-08-30 15:56:02 +0530

    Update SurfaceCpdf.php
    Change correct name space for CPdf class 
    \Svg\Surface\CPdf

--- src/Svg/Surface/SurfaceCpdf.php
+++ src/Svg/Surface/SurfaceCpdf.php
@@ -15,7 +15,7 @@ class SurfaceCpdf implements SurfaceInterface
 {
     const DEBUG = false;
 
-    /** @var \CPdf\CPdf */
+    /** @var \Svg\Surface\CPdf */
     private $canvas;
 
     private $width;
@@ -33,7 +33,7 @@ class SurfaceCpdf implements SurfaceInterface
         $h = $dimensions["height"];
 
         if (!$canvas) {
-            $canvas = new \CPdf\CPdf(array(0, 0, $w, $h));
+            $canvas = new \Svg\Surface\CPdf(array(0, 0, $w, $h));
             $refl = new \ReflectionClass($canvas);
             $canvas->fontcache = realpath(dirname($refl->getFileName()) . "/../../fonts/")."/";
         }
