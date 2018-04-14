679c22df298d00fcc5c3e8b1ea6e71b340d3918b
Barry vd. Heuvel <barryvdh@gmail.com>
2018-04-14 13:14:53 +0200

    Make sure $attributes is an array

--- src/Svg/Tag/AbstractTag.php
+++ src/Svg/Tag/AbstractTag.php
@@ -2,7 +2,7 @@
 /**
  * @package php-svg-lib
  * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
+ * @author  Fabien MÃ©nager <fabien.menager@gmail.com>
  * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
 
@@ -21,7 +21,7 @@ abstract class AbstractTag
     /** @var Style */
     protected $style;
 
-    protected $attributes;
+    protected $attributes = array();
 
     protected $hasShape = true;
 
