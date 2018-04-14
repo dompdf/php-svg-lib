cb610ede690a7981908ab24fbba4a3bed4019ca0
Barry vd. Heuvel <barryvdh@gmail.com>
2018-04-14 09:35:33 +0200

    Support (non-visible) desc attribute
    Prevent 'Unknown: desc'

--- src/Svg/Document.php
+++ src/Svg/Document.php
@@ -2,7 +2,7 @@
 /**
  * @package php-svg-lib
  * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
+ * @author  Fabien MÃ©nager <fabien.menager@gmail.com>
  * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
 
@@ -333,6 +333,9 @@ class Document extends AbstractTag
             case 'text':
                 $tag = new Text($this, $name);
                 break;
+
+            case 'desc':
+                return;
         }
 
         if ($tag) {
