b91b4614603fa69bb16b2c8a92992d9a116d139c
Brian Sweeney <bsweeney@aaas.org>
2024-03-16 12:06:07 -0400

    Update project link in file headers

--- src/Svg/Document.php
+++ src/Svg/Document.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package php-svg-lib
- * @link    http://github.com/PhenX/php-svg-lib
+ * @link    http://github.com/dompdf/php-svg-lib
  * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
  */
 
